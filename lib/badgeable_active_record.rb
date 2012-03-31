608740f3b6d339a5a5714051858fb4b359eaf53c
Scott Burton <scott@chaione.com>
2012-03-31 15:30:02 -0700

    use the #use adapter api

--- lib/badgeable_active_record.rb
+++ lib/badgeable_active_record.rb
@@ -1,5 +1,7 @@
+require 'badgeable'
+
 require "badgeable_active_record/version"
 require "badgeable_active_record/badge"
 require "badgeable_active_record/badging"
 
-Badgeable.adapter = :active_record
+Badgeable::Adapters.use :active_record
