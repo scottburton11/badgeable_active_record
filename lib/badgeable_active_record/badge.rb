83ccb76da4f25212a44e9fc1feaa02b1325c7dd3
Scott Burton <scott@chaione.com>
2012-03-31 22:05:09 -0700

    AR already has a find_or_create_by_name method

--- lib/badgeable_active_record/badge.rb
+++ lib/badgeable_active_record/badge.rb
@@ -1,9 +1,5 @@
 class Badge < ActiveRecord::Base
   has_many :badgings
-
-  def self.find_or_create_by_name(name)
-    Badge.where(:name => name).first || create(:name => name)
-  end
   
   def icon
     "/images/#{name.parameterize}.jpg"
