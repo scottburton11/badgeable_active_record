eb8a72b27bc31fcc0754475b6880933613f275c3
Scott Burton <scott@chaione.com>
2012-03-31 17:28:54 -0700

    require the adapter

--- lib/badgeable_active_record.rb
+++ lib/badgeable_active_record.rb
@@ -1,6 +1,7 @@
 require 'badgeable'
 
 require "badgeable_active_record/version"
+require "badgeable_active_record/adapter"
 require "badgeable_active_record/badge"
 require "badgeable_active_record/badging"
 
